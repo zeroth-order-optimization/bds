8d12c0c41df8607a3af9318bd42ccaadbbe16880
Lht97 <lhtian97@gmail.com>
2026-02-25 20:33:28 +0800

    Update profile_optiprofiler_script.m

--- tests/profile_optiprofiler_script.m
+++ tests/profile_optiprofiler_script.m
@@ -1,17 +1,20 @@
 clear all
-options.dim = 'large';
+options.dim = 'small';
 options.plibs = 's2mpj';
-options.feature_name = 'linearly_transformed';
-options.n_jobs = 1;
+options.feature_name = 'plain';
+% options.n_jobs = 1;
 
-options.solver_names = {'cbds', 'ds'};
+% options.solver_names = {'cbds', 'ds'};
 
-profile_optiprofiler(options);
+% profile_optiprofiler(options);
 
-options.solver_names = {'cbds', 'newuoa'};
+% options.solver_names = {'cbds', 'newuoa'};
 
-profile_optiprofiler(options);
+% profile_optiprofiler(options);
 
-options.solver_names = {'cbds', 'fd-bfgs'};
+% options.solver_names = {'cbds', 'fd-bfgs'};
 
+% profile_optiprofiler(options);
+
+options.solver_names = {'cbds', 'cbds-orig-direction-set-from-x0'};
 profile_optiprofiler(options);
